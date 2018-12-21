d11945da515e9abd45e71cbe2e5915b1e0d6292d
MudCitizen <mudcitizen@gmail.com>
2018-12-20 19:41:40 -0500

    Created stock-item component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,25 +9,17 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { StockItemComponent } from './stock/stock-item/stock-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    StockItemComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
-  ],
+    FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
