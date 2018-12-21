656c4d101a96ca297ab3d7b5d678adbb3c71ecef
MudCitizen <mudcitizen@gmail.com>
2018-12-20 20:45:15 -0500

    Finished Event Binding

--- ClientApp/src/app/stock/stock-item/stock-item.component.ts
+++ ClientApp/src/app/stock/stock-item/stock-item.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockItemComponent implements OnInit {
 
+  public name: string;
+  public code: string;
+  public price: number;
+  public previousPrice: number;
+  public positiveChange: boolean;
+  public favorite: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.name = "Test Stock Company";
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
+    this.positiveChange = this.price >= - this.previousPrice;
+    this.favorite = false;
+  }
+
+  toggleFavorite(event): void {
+    console.log("We are toggling the favorite state for this stock",event);
+    this.favorite = !this.favorite;
   }
 
 }
