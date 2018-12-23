df9e6ef66222efb690a30d0357825d06e6d21865
MudCitizen <mudcitizen@gmail.com>
2018-12-23 12:06:34 -0500

    After @Input

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Stock } from './model/stock.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Stock Market App';
+
+  public stockObj: Stock;
+
+  ngOnInit() {
+    this.stockObj = new Stock("Test Stock Co.", "TSC", 85, 80);
+  }
+
 }
--- ClientApp/src/app/stock/stock-item/stock-item.component.ts
+++ ClientApp/src/app/stock/stock-item/stock-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Stock } from "../../model/stock.model";
 
 @Component({
@@ -6,27 +6,15 @@ import { Stock } from "../../model/stock.model";
   templateUrl: './stock-item.component.html',
   styleUrls: ['./stock-item.component.css']
 })
-export class StockItemComponent implements OnInit {
+export class StockItemComponent {
 
-  public stocks: Array<Stock>;
+  @Input() public stock: Stock;
 
   constructor() { }
-
-  ngOnInit() {
-    this.stocks = [
-      new Stock("Test Stock", "TSC", 85, 60),
-      new Stock("Second Stock Company", "SSC", 10, 20),
-      new Stock("Last Stock Company", "LSC", 50.99, 69.99),
-    ]
-  }
-
+  
   toggleFavorite(event, index): void {
     console.log("We are toggling the favorite state for this stock", event, index);
-    this.stocks[index].favorite = !this.stocks[index].favorite;
-  }
-
-  handleSelection(event): void {
-    console.log("handleSelection() called",event);
+    this.stock.favorite = !this.stock.favorite;
   }
 
 }
